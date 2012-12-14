689c6656f1f57d759b7b9766e9b6d013e75b73b7
Avishai Ish-Shalom <avishai@fewbytes.com>
2012-12-14 15:21:21 +0200

    signal recipe should only work with chef_run_finished condition

--- recipes/signal.rb
+++ recipes/signal.rb
@@ -1,5 +1,9 @@
-chef_handler "Fewbytes::Chef::Handlers::CloudFormationSignalHandler" do
-  source "#{node['chef_handler']['handler_path']}/cfn_signal.rb"
-  arguments :url => node["cloudformation"]["metadata"]["Orchestration"]["ChefHandlerSignalURL"], :unique_id => node.name, :data => "Done running Chef"
-  action :enable
+if node["cloudformation"]["metadata"].has_key? "waitHandlers"
+  if node["cloudformation"]["metadata"]["waitHandlers"].has_key? "chef_run_finished"
+    chef_handler "Fewbytes::Chef::Handlers::CloudFormationSignalHandler" do
+      source "#{node['chef_handler']['handler_path']}/cfn_signal.rb"
+      arguments :url => node["cloudformation"]["metadata"]["waitHandlers"]["chef_run_finished"], :unique_id => node.name, :data => "Done running Chef"
+      action :enable
+    end
+  end
 end
